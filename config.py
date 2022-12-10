d3bc477e2f7ee5fe1674738d37b2f70a991c7066
Cameron <cameron.grams@gmail.com>
2022-12-09 22:28:19 -0500

    work on formating

---
+++ config.py
@@ -0,0 +1,9 @@
+# The config file is used to provide the necessary keys for downloading historical Tweets
+# This will require Academic Research access to the Twitter API
+# Instructions can be found: https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/full-archive-search
+
+API_Key =  "XXXXXXXXXXXXXXXX"
+
+API_Secret = "XXXXXXXXXXXXXXXX"
+
+Bearer_Token = "XXXXXXXXXXXXXXXX"
